67bc40cf6342940fcb5140c93f61299f3fbb0134
junhong625 <junhong625@naver.com>
2022-01-18 22:23:43 +0900

    Algorithm

---
+++ Algorithm23.py
@@ -0,0 +1,24 @@
+def divisors(num):
+    sum = 0
+    for j in range(1, num):
+        if i % j== 0:
+            sum += j
+    if sum > i:
+        abundant.append(num)
+
+abundant  = []
+for i in range(1, 28124):
+    divisors(i)
+
+all_numbers = set(range(1,28124))
+
+for abun1 in abundant:
+    for abun2 in abundant:
+        if abun1 + abun2 in all_numbers:
+            all_numbers.remove(abun1 + abun2)
+
+total = 0
+for i in range(len(all_numbers)):
+    total += list(all_numbers)[i]
+
+print(total)
